7eb44ac9d1215eed3be414200c4131efe0f6ee97
mtu <16877076+mtury@users.noreply.github.com>
2018-01-14 13:07:00 +0100

    Add fonts + Improve menu

--- static/js/base.js
+++ static/js/base.js
@@ -1,12 +1,28 @@
+var window_width_trigger = 1214;
 var errance_bar_trigger = document.getElementById("errance-bar-trigger");
 var errance_bar = document.getElementById("errance-bar");
+
 errance_bar_trigger.addEventListener("click", function(e) {
     e.preventDefault();
     if (errance_bar.classList.contains("expanded")) { 
         errance_bar.classList.remove("expanded");
-        /* $(".topic-bar-trigger").removeClass("active"); */
     } else {
         errance_bar.classList.add("expanded");
-        /* $(".topic-bar-trigger").addClass("active"); */
+    }
+
+    console.log(errance_bar.getAttribute('expanded_size'));
+
+    if (window.innerWidth < window_width_trigger) {
+        errance_bar.setAttribute("expanded_size", "double");
+    } else {
+        errance_bar.setAttribute("expanded_size", "simple");
+    }
+});
+
+window.addEventListener("resize", function(e) {
+    if (window.innerWidth < window_width_trigger) {
+        errance_bar.setAttribute("expanded_size", "double");
+    } else {
+        errance_bar.setAttribute("expanded_size", "simple");
     }
 });
