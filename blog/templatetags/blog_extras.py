cae3a76abd569f12e46caf805776cf8b606713d3
ria4 <16877076+ria4@users.noreply.github.com>
2018-01-29 01:53:23 +0100

    Blog style, login/logout blog, custom template tag for archive widget

---
+++ blog/templatetags/blog_extras.py
@@ -0,0 +1,18 @@
+
+from datetime import datetime
+from django import template
+from zinnia.models.entry import Entry
+
+register = template.Library()
+
+@register.simple_tag
+def get_article_on_day(date):
+    """
+    Do not use this on dates when no article was published!
+    """
+    entry = Entry.objects.filter(publication_date__year=date.year,
+                                 publication_date__month=date.month,
+                                 publication_date__day=date.day)[0]
+    url = '/blog/%.4d/%.2d/%.2d/%s}' % (date.year, date.month, date.day, entry.slug)
+    return {'title': entry.title, 'url': url}
+
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
+
+from django.contrib.auth import logout
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-# Create your views here.
+
+def logout_view(req):
+    if req.user.is_authenticated:
+        logout(req)
+    return HttpResponseRedirect(req.META.get('HTTP_REFERER'))
