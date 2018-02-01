9e51d9c3b983979aaa8445f520c5919273f77709
ria4 <16877076+ria4@users.noreply.github.com>
2018-02-01 01:31:38 +0100

    Handle comments graceful-ugh

---
+++ django_comments_custom/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DjangoCommentsCustomConfig(AppConfig):
+    name = 'django_comments_custom'
---
+++ django_comments_custom/forms.py
@@ -0,0 +1,30 @@
+
+"""
+/!\ This may break when upgrading django! /!\
+
+This whole mess of copy-paste cannot be avoided (?) because django wants to
+load all apps at once, therefore preventing easy imports from one app
+(contrib.contenttypes, django_comments) to another (django_comments_custom).
+
+But I'd rather copy-paste code than modify django app registry mechanisms...
+"""
+
+from ._forms import CommentForm
+
+class CommentFormCustom(CommentForm):
+
+    def get_comment_create_data(self, site_id=None):
+        from django.contrib.contenttypes.models import ContentType
+        return dict(
+            content_type=ContentType.objects.get_for_model(self.target_object),
+            object_pk=force_text(self.target_object._get_pk_val()),
+            user_name=self.cleaned_data["name"],
+            comment=self.cleaned_data["comment"],
+            submit_date=timezone.now(),
+            site_id=site_id or getattr(settings, "SITE_ID", None),
+            is_public=True,
+            is_removed=False,
+        )
+
+CommentFormCustom.base_fields.pop('url')
+CommentFormCustom.base_fields.pop('email')
