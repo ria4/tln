7784cb670e0ee3619f23341e5c983d87ebcb0bc4
mtu <16877076+mtury@users.noreply.github.com>
2018-02-02 15:40:51 +0100

    Comment moderation

--- blog/apps.py
+++ blog/apps.py
@@ -1,5 +1,15 @@
-from django.apps import AppConfig
+from django.apps import apps, AppConfig
+import django_comments
+from django_comments import signals
+from .moderator import EntryCommentModeratorCustom
 
 
 class BlogConfig(AppConfig):
     name = 'blog'
+
+    def ready(self):
+        from django_comments.moderation import moderator
+        moderator.unregister(apps.app_configs['zinnia'].get_model('Entry'))
+        moderator.register(apps.app_configs['zinnia'].get_model('Entry'),
+                           EntryCommentModeratorCustom)
+        signals.comment_was_flagged.connect(moderator.post_save_moderation, sender=django_comments.get_model())
--- django_comments_custom/forms.py
+++ django_comments_custom/forms.py
@@ -1,25 +1,29 @@
 
-"""
-/!\ This may break when upgrading django! /!\
-
-This whole mess of copy-paste cannot be avoided (?) because django wants to
-load all apps at once, therefore preventing easy imports from one app
-(contrib.contenttypes, django_comments) to another (django_comments_custom).
-
-But I'd rather copy-paste code than modify django app registry mechanisms...
-"""
-
 from django import forms
 from django.utils.encoding import force_text
 from django.utils.translation import pgettext_lazy, ugettext_lazy as _
 from django.utils import timezone
-from ._forms import CommentForm, COMMENT_MAX_LENGTH
+
+# django wants to load all apps at once, therefore preventing easy imports from
+# one app (contrib.contenttypes, django_comments) to another (the present one).
+from django.apps import apps
+if apps.apps_ready:
+    from django_comments.forms import CommentForm
+else:
+    apps.apps_ready = True
+    from django_comments.forms import CommentForm
+    apps.apps_ready = False
+
+COMMENT_MAX_LENGTH = 6000
+
 
 class CommentFormCustom(CommentForm):
-    name = forms.CharField(label=pgettext_lazy("Person name", "Name"), max_length=50, widget=forms.TextInput(attrs={"placeholder": "~nom~"}))
-    comment = forms.CharField(label=_('Comment'),
-                              widget=forms.Textarea(attrs={"placeholder": "~commentaire~"}),
-                              max_length=COMMENT_MAX_LENGTH)
+    name = forms.CharField(label=pgettext_lazy("Person name", "Name"), max_length=50,
+                           widget=forms.TextInput(attrs={"placeholder": "~nom~"}))
+    email = forms.EmailField(label=_("Email address"), required=False,
+                             widget=forms.EmailInput(attrs={"placeholder": "~email~ (si vous souhaitez être prévenu·e de futures réponses)"}))
+    comment = forms.CharField(label=_('Comment'), max_length=COMMENT_MAX_LENGTH,
+                              widget=forms.Textarea(attrs={"placeholder": "~commentaire~"}))
 
     def get_comment_create_data(self, site_id=None):
         from django.contrib.contenttypes.models import ContentType
@@ -27,6 +31,7 @@ class CommentFormCustom(CommentForm):
             content_type=ContentType.objects.get_for_model(self.target_object),
             object_pk=force_text(self.target_object._get_pk_val()),
             user_name=self.cleaned_data["name"],
+            user_email=self.cleaned_data["email"],
             comment=self.cleaned_data["comment"],
             submit_date=timezone.now(),
             site_id=site_id or getattr(settings, "SITE_ID", None),
@@ -35,4 +40,3 @@ class CommentFormCustom(CommentForm):
         )
 
 CommentFormCustom.base_fields.pop('url')
-CommentFormCustom.base_fields.pop('email')
