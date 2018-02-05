b5dc7398dc45fb91575d200a8c8499c588cde328
ria4 <16877076+ria4@users.noreply.github.com>
2018-02-05 12:20:11 +0100

    Remove some (hopefully) useless urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,24 @@
 
-#from django.contrib import admin
 from django.conf.urls import url
 from django.urls import include, path
 from . import views
 
-urlpatterns = [
-    path('', include('zinnia.urls')),
-    path('comments/', include('django_comments_custom.urls')),
-    #path('admin/', admin.site.urls),
+
+blog_urls = ([
+    #path('search/', include('zinnia.urls.search')),
+    path('tags/', include('zinnia.urls.tags')),
+    path('feeds/', include('zinnia.urls.feeds')),
+    path('', include('zinnia.urls.entries')),
+    path('', include('zinnia.urls.archives')),
+    path('', include('zinnia.urls.shortlink')),
+    path('', include('zinnia.urls.quick_entry')),
+    path('', include('zinnia.urls.capabilities')),
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/unsubscribe/(?P<h>[0-9a-f]{32})$',
         views.unsubscribe, name='unsubscribe'),
+], 'zinnia')
+
+urlpatterns = [
+    path('', include(blog_urls)),
+    path('comments/', include('django_comments_custom.urls')),
 ]
 
--- django_comments_custom/urls.py
+++ django_comments_custom/urls.py
@@ -1,11 +1,8 @@
-
-from django.conf.urls import url
 from django_comments.urls import urlpatterns as _urlpatterns
+from django.urls import path
 from .views.comments import post_comment
 
 
-urlpatterns = _urlpatterns[1:]
-
-urlpatterns += [
-    url(r'^post/$', post_comment, name='comments-post-comment'),
+urlpatterns = _urlpatterns[1:] + [
+    path('post/', post_comment, name='comments-post-comment')
 ]
