33ab967cdf26c6d538d417dfc6eb94ca9b5514ba
mtu <16877076+mtury@users.noreply.github.com>
2018-02-11 23:49:09 +0100

    Working on photos dropdown

--- critique/context_processors.py
+++ critique/context_processors.py
@@ -1,17 +1,15 @@
-
 from datetime import datetime
 from django.contrib.auth.forms import AuthenticationForm
 from .forms import OeuvreForm, OeuvreCommentForm
 
-def oeuvre_form_dispenser(req):
+def oeuvre_form(req):
     form = OeuvreForm(auto_id='id_empty_%s')
     return {"oeuvre_form_empty": form}
 
-def comment_form_dispenser(req):
+def comment_form(req):
     date = datetime.now().strftime('%Y-%m-%d')
     form = OeuvreCommentForm({'date': date}, auto_id='id_empty_%s')
     return {"comment_form_empty": form}
 
-def login_form_dispenser(req):
+def login_form(req):
     return {"login_form": AuthenticationForm()}
-
