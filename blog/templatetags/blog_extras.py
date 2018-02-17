c8583791276093ad3b3d1da5edb8059b6effc5aa
ria4 <16877076+ria4@users.noreply.github.com>
2018-02-17 04:04:02 +0100

    Superuser sensitive blog entries display

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -12,16 +12,53 @@ register = template.Library()
 
 
 @register.simple_tag
-def get_articles_on_day(date):
-    """
-    Do not use this on dates when no article was published!
-    """
-    entries = Entry.objects.filter(publication_date__year=date.year,
-                                   publication_date__month=date.month,
-                                   publication_date__day=date.day)
-    articles = [{'title': entry.title, 'url': '/blog/%s' % entry.slug}
-                 for entry in entries]
-    return articles
+def get_entries_on_year_length(date, is_superuser=False):
+    # Do not use this on dates when no entry was published!
+    if is_superuser is True:
+        queryset = Entry.objects
+    else:
+        queryset = Entry.published
+    entries = queryset.filter(publication_date__year=date.year)
+    return len(entries)
+
+@register.simple_tag
+def get_entries_on_month_length(date, is_superuser=False):
+    # Do not use this on dates when no entry was published!
+    if is_superuser is True:
+        queryset = Entry.objects
+    else:
+        queryset = Entry.published
+    entries = queryset.filter(publication_date__year=date.year,
+                              publication_date__month=date.month)
+    return len(entries)
+
+@register.simple_tag
+def get_entries_on_day(date, is_superuser=False):
+    # Do not use this on dates when no entry was published!
+    if is_superuser is True:
+        queryset = Entry.objects
+    else:
+        queryset = Entry.published
+    entries = queryset.filter(publication_date__year=date.year,
+                              publication_date__month=date.month,
+                              publication_date__day=date.day)
+    entries_clean = [{'title': entry.title, 'url': '/blog/%s' % entry.slug}
+                     for entry in entries]
+    return entries_clean
+
+
+@register.inclusion_tag('zinnia/tags/dummy.html', takes_context=True)
+def get_archives_entries_tree_su_sensitive(context,
+        template='zinnia/tags/entries_archives_tree.html'):
+    if context['request'].user.is_superuser:
+        queryset = Entry.objects
+    else:
+        queryset = Entry.published
+    return {'template': template,
+            'archives': queryset.datetimes(
+                'publication_date', 'day', order='ASC'),
+            'is_superuser': context['request'].user.is_superuser}
+
 
 @register.inclusion_tag('zinnia/tags/dummy.html', takes_context=True)
 def get_tag_cloud_su_sensitive(context, steps=6, min_count=None,
