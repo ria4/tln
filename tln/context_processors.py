4f4b90fa34e78f498bfc7edde7169d82acbe5081
ria4 <16877076+ria4@users.noreply.github.com>
2018-03-11 16:36:53 +0100

    Add android detection for archives widget

--- tln/context_processors.py
+++ tln/context_processors.py
@@ -2,3 +2,6 @@ from django.contrib.auth.forms import AuthenticationForm
 
 def login_form(req):
     return {"login_form": AuthenticationForm()}
+
+def android(req):
+    return {"android": "android" in req.META["HTTP_USER_AGENT"].lower()}
