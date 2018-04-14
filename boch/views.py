cb440c4fceb795a930eb30e901a13cdf3b851083
mtu <16877076+mtury@users.noreply.github.com>
2018-04-14 16:30:57 +0900

    Add china boxoffice

---
+++ boch/views.py
@@ -0,0 +1,35 @@
+import json, subprocess, sys
+from datetime import date, timedelta
+
+from django.http import HttpResponse
+
+
+def get_boch(req, day=None):
+    if day is None:
+        d = date.today() - timedelta(6)
+        day = d.strftime('%Y%m%d')
+
+    subprocess.call(['scrapy runspider /home/max/tln/boch/ent_spider.py -t json --nolog -a date="%s" -o - > /home/max/tln/static/boch/boxoffice_ch.json' % day], shell=True)
+
+    bo = {}
+    with open('static/boch/boxoffice_ch.json', 'r') as f:
+        lines = f.readlines()[1:-1]
+        for line in lines:
+            try:
+                info = json.loads(line[:-2])
+            except ValueError:
+                info = json.loads(line[:-1])
+            title = info['title']
+            num = info['num']
+            if title in bo:
+                bo[title] += int(num)
+            else:
+                bo[title] = int(num)
+    
+    s = ''
+    boo = [(k,bo[k]) for k in sorted(bo, key=bo.get, reverse=True)]
+    print(boo)
+    for i, (key, val) in enumerate(boo):
+        s += '%s. %s (%s)<br/>' % (i+1, key, val)
+
+    return HttpResponse(s)
