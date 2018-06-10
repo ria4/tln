def213f69da3ebf90e5354237ed3c1d4b9087299
mtu <16877076+mtury@users.noreply.github.com>
2018-06-10 17:16:04 +0900

    Update photos descriptions paddings

--- tln/urls.py
+++ tln/urls.py
@@ -15,3 +15,12 @@ urlpatterns = [
     path('login', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
 ]
+
+
+"""
+# serving media files in debug mode
+
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+"""
