40e7c452bc8ea7155cd9e18238fb4300b506b9ac
ria4 <16877076+ria4@users.noreply.github.com>
2018-07-25 23:27:44 +0900

    Add lang support for blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
-# Register your models here.
+from zinnia.admin.entry import EntryAdmin
+from zinnia.models.entry import Entry
+from .models import EntryLang
+
+
+class EntryLangAdmin(EntryAdmin):
+    fieldsets = (
+        (_('Content'), {
+            'fields': (('title', 'status'), 'lang', 'lead', 'content',)}),) + \
+                    EntryAdmin.fieldsets[1:]
+
+admin.site.register(Entry, EntryLangAdmin)
