68efb06a041b2b64d65c8212cf416ae9f712702d
ria4 <16877076+ria4@users.noreply.github.com>
2019-01-28 01:24:13 +0100

    Add max mail warnings to tln watcher

--- util/tln_watcher.sh
+++ util/tln_watcher.sh
@@ -1,21 +1,34 @@
 RECEIVERS="receiver1@mail.com receiver2@mail.com"
+MAX_MAILS=3
 HTTPS_URL="https://oriane.ink"
 CURL_CMD="curl -I -s -o /dev/null -w %{http_code}"
 CURL_MAX_CONNECTION_TIMEOUT="-m 30"
+
+TLN_WATCHER_CNT=$(cat /root/tln_watcher.cnt)
 
 CURL_RETURN_CODE=0
 CURL_OUTPUT=`${CURL_CMD} ${CURL_MAX_CONNECTION_TIMEOUT} ${HTTPS_URL} 2> /dev/null` || CURL_RETURN_CODE=$?
 
 if [ ${CURL_RETURN_CODE} -ne 0 ]; then
-    echo "Échec cURL sur oriane.ink. Reboot secteur conseillé !" | \
-        mail -s "Fail cURL oriane.ink" \
-             -a "From: TLN watcher <tln.watcher@gmail.com>" -a "Content-Type: text/plain; charset=UTF-8" \
-             ${RECEIVERS}
-else
-    if [ ${CURL_OUTPUT} -ne 200 ]; then
-        echo "Anomalie HTTP sur oriane.ink (code ${CURL_OUTPUT}). Une intervention semble de mise." | \
-            mail -s "Fail HTTP oriane.ink" \
+    if [ ${TLN_WATCHER_CNT} -lt ${MAX_MAILS} ]; then
+        ((TLN_WATCHER_CNT++))
+        echo "Échec cURL sur oriane.ink. Reboot secteur conseillé !" | \
+            mail -s "Fail cURL oriane.ink" \
                  -a "From: TLN watcher <tln.watcher@gmail.com>" -a "Content-Type: text/plain; charset=UTF-8" \
                  ${RECEIVERS}
     fi
+else
+    if [ ${CURL_OUTPUT} -ne 200 ]; then
+	if [ ${TLN_WATCHER_CNT} -lt ${MAX_MAILS} ]; then
+            ((TLN_WATCHER_CNT++))
+            echo "Anomalie HTTP sur oriane.ink (code ${CURL_OUTPUT}). Une intervention semble de mise." | \
+                mail -s "Fail HTTP oriane.ink" \
+                     -a "From: TLN watcher <tln.watcher@gmail.com>" -a "Content-Type: text/plain; charset=UTF-8" \
+                     ${RECEIVERS}
+        fi
+    else
+        TLN_WATCHER_CNT=0
+    fi
 fi
+
+echo ${TLN_WATCHER_CNT} > /root/tln_watcher.cnt
