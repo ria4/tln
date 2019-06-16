9e5720f2a98631e290d1e9de57ed1592043a1ae8
ria4 <16877076+ria4@users.noreply.github.com>
2019-06-17 00:41:09 +0200

    Login prompt for narrow devices

--- tln/context_processors.py
+++ tln/context_processors.py
@@ -3,6 +3,10 @@ from django.contrib.auth.forms import AuthenticationForm
 def login_form(req):
     f = AuthenticationForm()
     f.fields["username"].label = "Identifiant"
+    mobile_attrs = { "autocomplete": "off",
+                     "autocapitalize": "off",
+                     "spellcheck": "false" }
+    f.fields["username"].widget.attrs.update(mobile_attrs)
     return {"login_form": f}
 
 def android(req):
