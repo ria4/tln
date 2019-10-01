b1224d2008ce380482d7021a083cf23f06b50e91
ria4 <16877076+ria4@users.noreply.github.com>
2019-10-01 20:35:57 +0200

    Update gallery order in nav menu

--- photos/context_processors.py
+++ photos/context_processors.py
@@ -1,5 +1,5 @@
 from .models import GalleryCustom
 
 def gallery_list(req):
-    gallery_list = GalleryCustom.objects.filter(gallery__is_public=True).order_by("date_shooting")
+    gallery_list = GalleryCustom.objects.filter(gallery__is_public=True).order_by("gallery__date_added")
     return {"gallery_list": gallery_list}
