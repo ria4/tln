eeb3cd8c7af9c283befa7b58c6eb1b275b81bb52
ria4 <16877076+ria4@users.noreply.github.com>
2019-12-11 18:46:57 +0100

    WIP photos sidebar

--- photos/templatetags/photos_extras.py
+++ photos/templatetags/photos_extras.py
@@ -1,5 +1,7 @@
 from django import template
 
+from photos.models import GalleryCustom
+
 
 register = template.Library()
 
@@ -13,3 +15,31 @@ def stripgallery(title):
     if idx < 0:
         return title
     return title[idx+2:]
+
+@register.inclusion_tag('photologue/tags/galleries.html')
+def get_public_photo_galleries():
+    """
+    Return all public galleries as an HTML ul element.
+    """
+    return {'galleries': GalleryCustom.objects.filter(gallery__is_public=True)}
+
+def get_private_photo_galleries_array(user):
+    galleries_private = GalleryCustom.objects.filter(gallery__is_public=False)
+    if user.is_superuser:
+        return galleries_private
+    else:
+        return [gal for gal in galleries_private if user in gal.allowed_users.all()]
+
+@register.simple_tag(takes_context=True)
+def get_private_photo_galleries_num(context):
+    """
+    Return the number of private galleries accessible to the user.
+    """
+    return len(get_private_photo_galleries_array(context['user']))
+
+@register.inclusion_tag('photologue/tags/galleries_private.html', takes_context=True)
+def get_private_photo_galleries(context):
+    """
+    Return all private galleries accessible to the user as an HTML ul element.
+    """
+    return {'galleries': get_private_photo_galleries_array(context['user'])}
