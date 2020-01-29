aee2ab29115978e639257890b76022ff913cb6f8
ria4 <16877076+ria4@users.noreply.github.com>
2020-01-29 19:44:15 +0100

    Remove media debug service

--- tln/urls.py
+++ tln/urls.py
@@ -17,8 +17,10 @@ urlpatterns = [
 ]
 
 
+"""
 # serving media files in debug mode
 
 from django.conf.urls.static import static
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+"""
