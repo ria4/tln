403da681a9388f200e8ff9e08267fdd4110ee484
ria4 <16877076+ria4@users.noreply.github.com>
2020-02-13 21:13:32 +0100

    Better access-based blog entries integration

--- photos/urls.py
+++ photos/urls.py
@@ -10,10 +10,6 @@ from .views import GalleryCustomDetailView
 
 
 photos_urls = ([
-    url(r'^$', RedirectView.as_view(url=reverse_lazy('photologue:pl-gallery',
-                                                     kwargs={'slug':'tenacite'})),
-        name='pl-photologue-root'),
-    #url(r'^all$', GalleryListView.as_view(), name='gallery-list'),
     url(r'^(?P<slug>[\-\d\w]+)$', GalleryCustomDetailView.as_view(), name='pl-gallery'),
     url(r'^photo/(?P<slug>[\-\d\w]+)/$', permission_required('photos.all_rights')(PhotoDetailView.as_view()), name='pl-photo'),
 ], 'photologue')
