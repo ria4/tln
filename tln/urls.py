bce5d7148e7503f8b2155ccbfc62a8f9cd8708d1
ria4 <16877076+ria4@users.noreply.github.com>
2020-02-18 06:58:15 +0100

    Use DEBUG setting for serving media files

--- tln/urls.py
+++ tln/urls.py
@@ -2,7 +2,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+
 from . import views
+
 
 urlpatterns = [
     path('', include('home.urls')),
@@ -17,10 +19,6 @@ urlpatterns = [
 ]
 
 
-"""
-# serving media files in debug mode
-
-from django.conf.urls.static import static
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-"""
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
