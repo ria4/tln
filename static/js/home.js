92d3b0957f255c33aa11f572addc7c2a24fd7a5c
ria4 <16877076+ria4@users.noreply.github.com>
2021-11-24 20:01:20 +0100

    Add animation on homepage

--- static/js/home.js
+++ static/js/home.js
@@ -7,3 +7,28 @@ $('a[href^="#"]').click( function (event) {
         scrollTop: $(href).offset().top
     }, 500, function () { window.location.hash = href; });
 });
+
+
+/* Start blurring animation when info appears in viewport */
+
+if (!isTouchDevice()) {
+    function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return (rect.top <= window.innerHeight);
+    }
+
+    var infoContent = document.getElementById("info-content");
+
+    function infoHandler() {
+        if (!infoContent.classList.contains("anim-blur")) {
+            var visible = isElementInViewport(infoContent);
+            if (visible) {
+                infoContent.classList.add("anim-blur");
+            }
+        }
+    }
+
+    window.addEventListener('load', infoHandler, false);
+    window.addEventListener('scroll', infoHandler, false);
+    window.addEventListener('resize', infoHandler, false);
+}
