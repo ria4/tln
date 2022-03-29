f861fb9a385e4fb4f7313c1a8ea4cc2f699757e1
ria4 <16877076+ria4@users.noreply.github.com>
2022-03-29 22:00:43 +0200

    Upgrade critiquedate filter to fancydate tag

--- tln/templatetags/tln_extras.py
+++ tln/templatetags/tln_extras.py
@@ -1,7 +1,9 @@
 import string
 
 from django import template
-
+from django.utils.dateformat import format as date_format
+from django.utils.safestring import mark_safe
+from django.utils.timezone import localtime
 
 register = template.Library()
 
@@ -20,3 +22,36 @@ def truncatedesc(arg1, arg2):
     while res[-1] in string.punctuation:
         res = res[:-1]
     return res + "..."
+
+@register.simple_tag
+def fancydate(obj, date_attrname='date', en=False, le=False, annee=False):
+    """Return a pretty date, e.g. for a Seance or a Commentaire.
+
+    Django would handle localization in one of its templates,
+    but here it has to be done manually."""
+
+    dt = localtime(getattr(obj, date_attrname))
+    res = ""
+
+    if getattr(obj, 'date_month_unknown', None):
+        if en:
+            res += "en "
+        res += f"{date_format(dt, 'Y')}"
+        return res
+
+    if getattr(obj, 'date_day_unknown', None):
+        if en:
+            res += "en "
+        res += f"{date_format(dt, 'F Y')}"
+        return res
+
+    if le:
+        res += "le "
+    res += date_format(dt, 'j')
+    if dt.day == 1:
+        res += "<sup>er</sup>"
+    res += f" {date_format(dt, 'F')}"
+    if annee:
+        res += f" {date_format(dt, 'Y')}"
+
+    return mark_safe(res)
