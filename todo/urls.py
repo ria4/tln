ee2cf3c7ad70c20c633c3984cd2d4e87ac91ab11
ria4 <16877076+ria4@users.noreply.github.com>
2022-06-10 09:19:38 +0200

    Update todo paths

--- todo/urls.py
+++ todo/urls.py
@@ -5,10 +5,11 @@ from todo import views
 
 urlpatterns = [
     path('', views.TodoListListView.as_view(), name='index'),
-    path('list/<int:list_id>/', views.TodoItemListView.as_view(), name='list'),
-    path('list/<int:pk>/delete/', views.TodoListDeleteView.as_view(), name='list-delete'),
-    path('list/add/', views.TodoListCreateView.as_view(), name='list-add'),
-    path('list/<int:list_id>/item/add/', views.TodoItemCreateView.as_view(), name='item-add'),
-    path('list/<int:list_id>/item/<int:pk>', views.TodoItemUpdateView.as_view(), name='item-update'),
-    path('list/<int:list_id>/item/<int:pk>/delete/', views.TodoItemDeleteView.as_view(), name='item-delete'),
+    path('lists', views.TodoListListView.as_view()),
+    path('lists/<int:list_id>/', views.TodoItemListView.as_view(), name='list'),
+    path('lists/<int:pk>/delete/', views.TodoListDeleteView.as_view(), name='list-delete'),
+    path('lists/add/', views.TodoListCreateView.as_view(), name='list-add'),
+    path('lists/<int:list_id>/items/add/', views.TodoItemCreateView.as_view(), name='item-add'),
+    path('lists/<int:list_id>/items/<int:pk>', views.TodoItemUpdateView.as_view(), name='item-update'),
+    path('lists/<int:list_id>/items/<int:pk>/delete/', views.TodoItemDeleteView.as_view(), name='item-delete'),
 ]
