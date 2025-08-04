16b51518c146e3406481cf189b636a3e0c0f6d5b
ria4 <16877076+ria4@users.noreply.github.com>
2025-08-04 20:39:42 +0200

    Remove broken homepage blur animation

--- home/static/home/home.js
+++ home/static/home/home.js
@@ -7,34 +7,3 @@ $('a[href^="#info"]').click( function (event) {
         scrollTop: $(href).offset().top
     }, 500, function () { window.location.hash = href; });
 });
-
-
-/* Start blurring animation when info appears in viewport */
-
-if (!isTouchDevice()) {
-    function isElementInViewport(el) {
-        var rect = el.getBoundingClientRect();
-        return (rect.top <= window.innerHeight);
-    }
-
-    var infoContent = document.getElementById("info-content");
-    var infoSeparator = document.getElementById("info-separator");
-
-    function infoHandler() {
-        if (!infoContent.classList.contains("anim-blur")) {
-            var visible = isElementInViewport(infoSeparator);
-            if (visible) {
-                infoContent.classList.add("anim-blur");
-            }
-        }
-    }
-
-    window.addEventListener('load', infoHandler, false);
-    window.addEventListener('scroll', infoHandler, false);
-    window.addEventListener('resize', infoHandler, false);
-} else {
-    var infoClones = document.getElementsByClassName("onhover-display");
-    while(infoClones.length > 0) {
-        infoClones[0].parentNode.removeChild(infoClones[0]);
-    }
-}
